6e16024d51797d2c8442c6b6d21e4d78af4ec08c
Adithya Ganesh <adithya.cganesh@gmail.com>
2016-11-25 17:04:38 -0800

    Move data utils, add early stopping + model checkpointing

--- models/lstm_basic.py
+++ models/lstm_basic.py
@@ -4,26 +4,11 @@ import scipy.io
 from keras.models import Sequential
 from keras.layers import Bidirectional, LSTM
 from keras.layers.core import Dropout, Dense
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 
-np.random.seed(42)
+from data_utils import load
 
-DATA_PATH = '/home/data/deepsea_train'
-print 'loading data'
-trainmat = h5py.File('%s/train.mat' % DATA_PATH)
-validmat = scipy.io.loadmat('%s/valid.mat' % DATA_PATH)
-testmat = scipy.io.loadmat('%s/test.mat' % DATA_PATH)
-
-print 'data loaded'
-
-#X_train.shape: (4400000, 1000, 4)
-#y_train.shape: (4400000, 919)
-# 4400000 examples; each example is 1000 x 4 (1000 bp, one-hot encoding for each base)
-# Output is length 919 binary prediction task
-
-X_train = np.transpose(np.array(trainmat['trainxdata']), axes=(2, 0, 1))
-y_train = np.array(trainmat['traindata']).T
-
-print 'data np-ified'
+X_train, y_train = load()
 
 model = Sequential()
 model.add(Bidirectional(LSTM(320, return_sequences=False), input_shape=(1000, 4)))
@@ -31,7 +16,11 @@ model.add(Dropout(0.5))
 model.add(Dense(output_dim=919, activation='sigmoid'))
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', class_mode='binary')
 
+checkpointer = ModelCheckpoint(filepath="odel.hdf5", verbose=1, save_best_only=True)
+earlystopper = EarlyStopping(monitor='val_loss', patience=5, verbose=1)
 
 model.fit(X_train, y_train, batch_size=100, nb_epoch=60, shuffle=True, show_accuracy=True, validation_data=(np.transpose(validmat['validxdata'],axes=(0,2,1)), validmat['validdata']))
 
 tresults = model.evaluate(np.transpose(testmat['testxdata'],axes=(0,2,1)), testmat['testdata'],show_accuracy=True)
+
+print tresults
