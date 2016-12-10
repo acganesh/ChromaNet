0c2ac3349d425522feea1ed17b41dd808f8bdc9c
Armin <apoursh@galangal.stanford.edu>
2016-12-10 10:35:49 -0800

    reweighted but buggy

--- deepshit.py
+++ deepshit.py
@@ -28,7 +28,7 @@ _EPSILON = 10e-8
 
 random.seed('123')
 
-sampled = 500
+sampled = 5
 index = random.sample(xrange(919), sampled)
 batch_sz = 16
 
@@ -53,6 +53,9 @@ os.shape = (batch_sz * sampled,1)
 zs.shape = (batch_sz * sampled,1)
 ones, zeros = os, zs 
 
+y_train = y_train[:16, :]
+X_train = X_train[:16,:,:]
+
 #meanOnes = np.mean(y_train, 0)
 #w = np.array([1.0/item for item in meanOnes])
 #meanW = np.mean(w)
@@ -68,9 +71,12 @@ ones, zeros = os, zs
 def NLL_loss(y_true, y_pred):
     y_pred = T.clip(y_pred, _EPSILON, 1.0 - _EPSILON)
     y_t = K.reshape(y_true,(batch_sz * sampled,1))
-    weighted = K.prod(K.concatenate([y_t,zeros], axis = 1), axis=1)
-    y_t = K.reshape(weighted,(batch_sz, sampled))
-    return -(y_t * K.log(y_pred) + (1.0-y_t) * K.log(1.0 - y_pred))
+    one_weights  = K.prod(K.concatenate([y_t,zeros], axis = 1), axis=1)
+    zero_weights = K.prod(K.concatenate([1.0-y_t, ones], axis=1), axis=1)
+    z_weights = K.reshape(zero_weights,(batch_sz, sampled))
+    o_weights = K.reshape(one_weights ,(batch_sz, sampled))
+    #assert K.dot(y_t, 1.0-y_t)==K.dot(y_t, 1.0-y_t) 
+    return -(o_weights * K.log(y_pred) + z_weights * K.log(1.0 - y_pred))
 
 
 lr, decay, p = 0.005, 8e-7, 0.5
@@ -79,10 +85,10 @@ filters = [320, 480, 960]
 width = 1000
 convWindows = [8, 8, 8]
 poolWindows = [4,4,4]
-dropouts    = [0.2,0.2,0.3,0.4]
+dropouts    = [0.0,0.0,0.0,0.0]
 num_layers = 3
 
-thef = open('Behrooz.txt', 'a')
+thef = open('test.txt', 'a')
 
 model = Sequential()
 
@@ -142,11 +148,11 @@ class Histories(Callback):
 h1 = Histories()
 
 
-checkpointer = ModelCheckpoint(filepath="weights/weights.{epoch:02d}-{val_loss:.2f}_"+str(lr)+"_NewWeighting_Nadam.hdf5", verbose=1, save_best_only=False,save_weights_only=False)
-earlystopper = EarlyStopping(monitor='val_loss', patience=4, verbose=1, mode='min')
+checkpointer = ModelCheckpoint(filepath="weights/weightstest.{epoch:02d}-{val_loss:.2f}_"+str(lr)+"_NewWeighting_Nadam.hdf5", verbose=1, save_best_only=False,save_weights_only=False)
+earlystopper = EarlyStopping(monitor='val_loss', patience=40, verbose=1, mode='min')
 
 
-history = model.fit(X_train, y_train, batch_size=batch_sz, nb_epoch=12, shuffle=True, verbose = 1, validation_data=(np.transpose(validmat['validxdata'],axes=(0,2,1)), validmat['validdata'][:,index]), callbacks=[checkpointer,earlystopper,h1], show_accuracy=True)
+history = model.fit(X_train, y_train, batch_size=batch_sz, nb_epoch=120, shuffle=True, verbose = 1, validation_data=(np.transpose(validmat['validxdata'],axes=(0,2,1))[:16,:,:], validmat['validdata'][:16,index]), callbacks=[checkpointer,earlystopper,h1], show_accuracy=True)
 
 print >> thef, "lr=",lr, decay, p, 'Nadam default'
 print >> thef, history.history['val_loss'], history.history['val_binary_accuracy']
