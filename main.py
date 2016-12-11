29a353243614b5cf8adb17fec66cb68b69e4ebc3
Adithya Ganesh <acganesh@stanford.edu>
2016-12-10 22:28:58 -0800

    Run medium size experiment

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import os
 import numpy as np
 from keras.callbacks import ModelCheckpoint, EarlyStopping
-
+from keras.optimizers import RMSprop
 
 from data_utils import load
 from models import BidirectionalLSTMNet
@@ -19,15 +19,19 @@ np.random.seed(42)
 def main():
     # Disable output buffering
     #sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0) 
+    learning_rate = 0.001
 
-    X_train_tiny, y_train_tiny = load('tiny')
+    X_train, y_train = load('med')
+    print 'Data loaded'
 
     net = BidirectionalLSTMNet()
     model = net.build_model()
 
-    checkpointer = ModelCheckpoint(filepath='./experiments/lstm_test.hdf5', verbose=1, save_best_only=True)
+    checkpointer = ModelCheckpoint(filepath='./experiments/lstm_test_med.hdf5', verbose=1, save_best_only=True)
     earlystopper = EarlyStopping(monitor='val_loss', patience=3, verbose=1)
-    model.fit(X_train_tiny, y_train_tiny, batch_size=100, class_weight='auto', nb_epoch=10, shuffle=True, validation_split=0.2, callbacks=[checkpointer, earlystopper])
+    rmsprop = RMSprop(lr = learning_rate)
+    model.compile(loss='binary_crossentropy', optimizer=rmsprop)
+    model.fit(X_train, y_train, batch_size=100, class_weight='auto', nb_epoch=10, shuffle=True, validation_split=0.2, callbacks=[checkpointer, earlystopper])
 
 if __name__ == '__main__':
     main()
